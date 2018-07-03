e636fec55fff5679cce856db892cf22c8bd05307
Ayuki <agathe.lefevre@viacesi.fr>
2018-07-03 09:34:23 +0200

    CalculatedDB & Calculator  useless commentaries removed

--- CalculatedDB/Calculator.cs
+++ CalculatedDB/Calculator.cs
@@ -32,42 +32,5 @@ namespace CalculatedDB
         }
         
 
- /*       private static IEnumerable<BsonDocument> DocumentCreation()
-        {
-            var device1 = new BsonDocument
-                {
-                  {"ID", "1"},
-                  {"DateTime", "10/12/2015"},
-                  {"DataType", "Temperature"},
-                  {"Value", 37},
-                  {"DeviceType", "TemperatureSensor"}
-                };
-
-            var device2 = new BsonDocument
-                {
-                  {"ID", "2"},
-                  {"DateTime", "29/06/2018"},
-                  {"DataType", "Brightness"},
-                  {"Value", 15},
-                  {"DeviceType", "BrightnessSensor"}
-                };
-
-            var device3 = new BsonDocument
-                {
-                  {"ID", "3"},
-                  {"DateTime", "12/02/2002"},
-                  {"DataType", "Humidity"},
-                  {"Value",  20},
-                  {"DeviceType", "HumiditySensor"}
-                };
-
-            var newDevices = new List<BsonDocument>();
-            newDevices.Add(device1);
-            newDevices.Add(device2);
-            newDevices.Add(device3);
-
-
-            return newDevices;
-        }*/
     }
 }
